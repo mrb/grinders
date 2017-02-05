f55e92947ee0abe0d8b69dba8d924d3b48b74f4a
Michael Bernstein <michaelrbernstein@gmail.com>
2017-02-05 18:44:13 -0500

    Ses

---
+++ config/initializers/ses.rb
@@ -0,0 +1,22 @@
+require 'net/smtp'
+
+module Net
+  # Force TLS over SMTP
+  class SMTP
+    def tls?
+      true
+    end
+  end
+end
+
+Pizzabookbackers::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'email-smtp.us-east-1.amazonaws.com',
+    authentication: :login,
+    user_name: ENV['SES_USERNAME'],
+    password: ENV['SES_PASSWORD'],
+    enable_starttls_auto: true,
+    port: 465
+  }
+end
