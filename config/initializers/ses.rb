2135a5adf46c38be665d9fac6ff0ee71d413bd51
Michael Bernstein <michaelrbernstein@gmail.com>
2017-02-05 18:46:23 -0500

    Rails

--- config/initializers/ses.rb
+++ config/initializers/ses.rb
@@ -9,7 +9,7 @@ module Net
   end
 end
 
-Grinders::Application.configure do
+Rails::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'email-smtp.us-east-1.amazonaws.com',
