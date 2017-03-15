2c2bbbd7cd207acd03e0f5fe51b4ec5d7c165d5c
Michael Bernstein <michaelrbernstein@gmail.com>
2017-03-15 15:21:45 -0400

    Twitter and fonts

--- config/initializers/production.rb
+++ config/initializers/production.rb
@@ -1,11 +1,16 @@
 class << Rails.application
-    def domain
-      "news.reifyworks.com"
-    end
-
-    def name
-      "Market News"
-    end
+  def domain
+    "news.reifyworks.com"
   end
 
-  Rails.application.routes.default_url_options[:host] = Rails.application.domain
+  def name
+    "Market News"
+  end
+end
+
+Rails.application.routes.default_url_options[:host] = Rails.application.domain
+
+Twitter.CONSUMER_KEY = ENV["CONSUMER_KEY"]
+Twitter.CONSUMER_SECRET = ENV["CONSUMER_SECRET"]
+Twitter.AUTH_TOKEN = ENV["AUTH_TOKEN"]
+Twitter.AUTH_SECRET = ENV["AUTH_SECRET"]
